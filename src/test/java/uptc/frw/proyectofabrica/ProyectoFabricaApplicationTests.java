77c4c08d54cd54c873b31fc929f0ee19fe1e0d80
mzambrano <mzambrano@homecenter.co>
2024-06-11 00:07:44 -0500

    commit proyecto inicial

---
+++ src/test/java/uptc/frw/proyectofabrica/ProyectoFabricaApplicationTests.java
@@ -0,0 +1,13 @@
+package uptc.frw.proyectofabrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFabricaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
